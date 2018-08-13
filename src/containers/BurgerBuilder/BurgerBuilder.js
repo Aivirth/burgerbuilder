d3c2f362813ccfd88e0dfc5e92141fdfa2fa038f
Aivirth <aivirth.work@gmail.com>
2018-08-13 16:36:07 +0200

    Added burger ingredients rendering based on they values

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,19 @@ import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 
 export default class BurgerBuilder extends Component {
-  state = {};
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 5,
+      meat: 0
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
