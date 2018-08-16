265045170112a4eb327ce045916f84293973e687
Aivirth <aivirth.work@gmail.com>
2018-08-16 17:26:41 +0200

    Added logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import burgerLogo from "../../assets/images/burger-logo.png";
+import classes from "./Logo.css";
+
+export default props => {
+  return (
+    <div className={classes.Logo}>
+      <img src={burgerLogo} alt="Burger Logo" />
+    </div>
+  );
+};
