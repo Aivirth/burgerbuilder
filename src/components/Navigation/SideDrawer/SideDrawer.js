01a0908c6c12c146513c9551a9f4f9aa680fc3af
Aivirth <aivirth.work@gmail.com>
2018-08-17 15:20:52 +0200

    Added SideDrawer component

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.css";
+
+export default props => {
+  return (
+    <div className={classes.SideDrawer}>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
