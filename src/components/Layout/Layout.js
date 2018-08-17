f8695c7b9812f54d96b78418ca99a0ad7942289c
Aivirth <aivirth.work@gmail.com>
2018-08-17 16:22:01 +0200

    Converted Layout to stateful component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => {
-  return (
-    <Aux>
-      <Toolbar />
-      <SideDrawer />
-      <main className={classes.Content}>{props.children}</main>
-    </Aux>
-  );
-};
-
-export default layout;
+export default class extends Component {
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 export default props => {
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Fragment>
+      <Backdrop show />
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Fragment>
   );
 };
