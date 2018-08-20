921a5a4aebbe8357f442b2401c004d797fd686ae
Aivirth <aivirth.work@gmail.com>
2018-08-20 11:13:38 +0200

    Used lifecycle hook on Modal component to check if OrderSummary Should rerender

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,17 +2,29 @@ import React from "react";
 import classes from "./Modal.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-export default props => (
-  <React.Fragment>
-    <Backdrop clicked={props.modalClosed} show={props.show} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
-  </React.Fragment>
-);
+export default class extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log("[modal] Will Update");
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop clicked={this.props.modalClosed} show={this.props.show} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
