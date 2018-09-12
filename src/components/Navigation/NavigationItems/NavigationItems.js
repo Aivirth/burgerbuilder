7a6de3c5efd299bd073182900d456f1cc8ced23b
Aivirth <aivirth.work@gmail.com>
2018-09-12 23:20:09 +0200

    Reworked navigation items to use Link components

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,10 +5,10 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 export default () => {
   return (
     <ul className={classes.NavigationItems}>
-      <NavigationItem link="/" active>
+      <NavigationItem link="/" exact>
         Burger Builder
       </NavigationItem>
-      <NavigationItem link="/checkout">Checkout</NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
   );
 };
