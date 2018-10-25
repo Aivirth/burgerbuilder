1d917606f01e420ec716c83f9c022e3fff81af54
Aivirth <aivirth.work@gmail.com>
2018-10-25 11:14:42 +0200

    Added order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,8 @@ import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
 import { connect } from "react-redux";
+import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
+import * as actions from "../../../store/actions/index";
 
 class ContactData extends Component {
   state = {
@@ -155,9 +157,6 @@ class ContactData extends Component {
 
   orderHandler = event => {
     event.preventDefault();
-    this.setState({
-      loading: true
-    });
 
     const formData = {};
     for (let formElementIdentifier in this.state.orderForm) {
@@ -171,20 +170,8 @@ class ContactData extends Component {
       price: this.props.totalPrice,
       orderData: formData
     };
-    axios
-      .post("/orders.json", order)
-      .then(res => {
-        this.setState({
-          loading: false
-        });
-        this.props.history.push("/");
-      })
-      .catch(err => {
-        this.setState({
-          loading: false
-        });
-        console.log(err);
-      });
+
+    this.props.onOrderBurger(order);
   };
 
   render() {
@@ -236,4 +223,11 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  onOrderBurger: orderData => dispatch(actions.purchaseBurgerStart(orderData));
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(ContactData, axios));
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,4 +3,8 @@ export {
   removeIngredient,
   initIngredients
 } from "./burgerBuilder";
-// export {} './order'
+export {
+  purchaseBurgerStart,
+  purchaseBurgerFail,
+  purchaseBurgerSuccess
+} from "./order";
