4eb804baec0ea6e89359284c108c8d1d43a17d31
Aivirth <aivirth.work@gmail.com>
2018-11-07 11:54:18 +0100

    Added additional test : 'should contain a NavigationItem pointing to logout if authenticated'

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -22,4 +22,11 @@ describe("<NavigationItems />", () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it("should contain a NavigationItem pointing to logout if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
 });
